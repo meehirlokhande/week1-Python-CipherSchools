c9b283670fca399ae9b66ebf29c70f374f2f6b45
meehirlokhande <116459433+meehirlokhande@users.noreply.github.com>
2022-12-01 19:25:59 +0530

    Add files via upload

---
+++ class6-Conditional Statements-if-elif-else-CipherSchools.py
@@ -0,0 +1,39 @@
+import math
+a=True
+
+if a:
+    print("the value is true")
+print("end")
+
+a=False
+if a:
+    print("this value is true")
+
+else:
+    print("this value is false")
+
+a=5
+if a==3:
+    print("this value is 3")
+elif a==5:
+    print("this value is 5")
+else:
+    print("this value is not 3 nor 5")
+
+#x: int(-inf,inf)
+#a=x<0
+#b=x==0
+#c=x>0
+
+#q= can profile A access profile a 
+#a = isfriend
+#b= isblocked
+#c=isadmin
+#d=ismarkzuckerberg
+
+#a b c d q
+#0 0 0 0 0
+#0 0 0 1 1
+#0 0 1 0 1
+#1 1 1 1 1
+
