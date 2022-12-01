5c537be3154229041189618382eed14062c2104f
meehirlokhande <116459433+meehirlokhande@users.noreply.github.com>
2022-12-01 19:25:41 +0530

    Add files via upload

---
+++ class5-Operators and Expressions-CipherSchools.py
@@ -0,0 +1,62 @@
+print(sum(1,2))
+#arithmetic operator
+print(5+5)
+
+print(10-5)
+
+print(10/2)
+
+print(10//3)
+
+print(10%3)
+
+print(2**6)
+
+print(2*2)
+
+print("abd"*3)
+
+#comparission operator
+
+print(1==2)
+
+print(1!=2)
+
+prin(1<2)
+
+print(2>3)
+
+print("a" < "A")
+
+#logical operators
+print(True and False)
+
+print(True or False)
+
+print(True and 1)
+
+print(1 and 0)
+
+print(1 and 5)
+
+print(isinstance(True,int))
+
+print(type(True))
+
+
+print("meehir" and 6)
+
+print(bool((1,2,3)))
+
+print(bool(""))
+
+#short curcuiting
+# print(a or b = a(if a is truthy))
+# print(a or b = b(if a id falsy))
+# print(a and b = b(if a is truthy))
+# print(a and b = a(if a is falsy))
+
+# print(true or b = true)
+# print(false or b = b)
+#  print(true and b = b)
+# print(false and b = false)
