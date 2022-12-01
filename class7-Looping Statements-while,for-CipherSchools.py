56f613118ec2972fadf14b37aea29874d28acd57
meehirlokhande <116459433+meehirlokhande@users.noreply.github.com>
2022-12-01 19:26:18 +0530

    Add files via upload

---
+++ class7-Looping Statements-while,for-CipherSchools.py
@@ -0,0 +1,45 @@
+a=1
+while a<10:
+    print(a)
+    a+=1
+
+
+battery=input()
+while battery.notDead():
+    pass   
+name = "meehir"
+name.__iter__
+print(type(name))
+print("***")
+for c in name:
+ print(c)
+print(type(c))
+
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    print(i)
+    if i==3:
+      break
+    
+for i in [0,1,2,3,4]:
+    print(i)
+    i=100
+    print(i)
+
+if True:
+    pass
+print("rest of the code")
+
+for i in range(5):
+    print(i)
+    if i==3:
+        break
+print("something")
+
+for i in range(5):
+    print(i)
+    if i==3:
+        continue
+
